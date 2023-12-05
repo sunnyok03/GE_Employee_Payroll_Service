fd92c83d6753694293f9999723702df6e989818a
sunnyok03 <sunnyok03@gmail.com>
2023-12-06 00:54:41 +0530

    UC1 added employeePayroll class

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeePayroll employeePayroll = new EmployeePayroll("1","Sunny",40000);
+        employeePayroll.writeEmployeePayroll();
+    }
+}
